44935725b59614462bdf4763ac6f2476eeda70f2
Javokhirbek Khaydaraliev <khaydaraliev99@gmail.com>
2021-06-26 17:11:00 +0500

    Added managing user unit tests and endpoints

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -17,11 +17,24 @@ class UserSerializer(serializers.ModelSerializer):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for creating token authentication"""
-    email = serializers.CharField()
+    email = serializers.CharField(label=_('Email'))
     password = serializers.CharField(
+        label=_('Password'),
         style={'input_style':'password'},
         trim_whitespace=False
     )
@@ -29,10 +42,10 @@ class AuthTokenSerializer(serializers.Serializer):
     def validate(self, attrs):
         email = attrs.get('email')
         password = attrs.get('password')
-
+        
         user = authenticate(
             request=self.context.get('request'),
-            email=email,
+            username=email,
             password=password
         )
 
